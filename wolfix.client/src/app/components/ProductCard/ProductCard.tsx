877cbe57dd69be05cae2f776a19c5b1ac73a81fe
Lolkash82 <149090430+Lolkash82@users.noreply.github.com>
2025-08-03 22:14:35 +0200

    upadre product card. Add icons

--- wolfix.client/src/app/components/ProductCard/ProductCard.tsx
+++ wolfix.client/src/app/components/ProductCard/ProductCard.tsx
@@ -13,17 +13,29 @@ const ProductCard: FC<IProductCardProps> = ({ name, oldPrice, price, rating, add
   return (
     <div className="product-card">
       <img src={imageSrc} alt={name} className="product-image" />
+      <div className="top-buttons">
+        <img src="/icons/Vector79.png" alt="Add to Favorites" className="button-icon" />
+        <img src="/icons/Group.png" alt="Add to Compare" className="button-icon" />
+      </div>
       <div className="product-info">
         <div className="product-name">{name}</div>
         <div className="product-details">
-          <div className="product-rating">★ {rating}</div>
-          <div className="product-prices">
-            <span className="product-old-price">{oldPrice} грн</span>
-            <span className="product-new-price">{price}</span>
+          <div className="product-pricing">
+            <div className="product-rating">
+              <img src="/icons/Vector.jpg" alt="Star" className="rating-star" />
+              <span className="rating-value">{rating}</span>
+            </div>
+            <div className="product-old-price">{oldPrice} грн</div>
+            <div className="product-new-price">{price}</div>
           </div>
-          <div className="product-fee">+ {additionalFee} бонусів</div>
         </div>
-        <button className="add-to-cart">🛒</button>
+        <div className="cart-button">
+          <img src="/icons/ShoppingCart.png" alt="Add to Cart" className="button-icon-cart" />
+        </div>
+        <div className="product-fee">
+          <img src="/icons/Coins.png" alt="Bonus" className="bonus-icons" />
+          + {additionalFee} бонусів
+        </div>
       </div>
     </div>
   );
